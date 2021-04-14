d7df4b175a3f568336f4d42bb271e90f55b0744a
D0745704 <mushroom512348@gmail.com>
2021-04-14 19:18:21 +0800

    Done

--- D0745704_hw/ContentView.swift
+++ D0745704_hw/ContentView.swift
@@ -8,9 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var username: String = ""
+    @State var password: String = ""
+    @State var sliderTemp = 18.0
+    @State private var selectedDegree = 0
+    @State private var ButtonSwitch = false
+    var ageMin = 18.0
+    var ageMax = 34.0
+    var typesOfDegree = ["undergraduate","master"]
+    private var enableButton: Bool {
+        !username.isEmpty && !password.isEmpty
+        && ButtonSwitch
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            HStack{
+                Text("Username")
+                TextField("Username",text: $username)
+                    .frame(width: 245.0)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+            }
+            HStack{
+                Text("Password")
+                SecureField("Password",text: $password)
+                    .frame(width:250.0)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+            }
+            HStack{
+                Text("Age:\(Int(ageMin))")
+                Slider(value: $sliderTemp,in: ageMin...ageMax)
+                    .frame(width: 210.0)
+                Text("Age:\(Int(ageMax))")
+                
+            }.padding()
+            Text("Your Age: \(Int(sliderTemp))")
+                .fontWeight(.bold)
+            HStack{
+                Picker(selection: $selectedDegree, label: /*@START_MENU_TOKEN@*/Text("Picker")/*@END_MENU_TOKEN@*/.bold()){
+                    ForEach(0..<typesOfDegree.count){
+                        Text(self.typesOfDegree[$0]).tag($0)
+                    }
+                }
+                .frame(width: 180.0, height: 100.0)
+            }
+            Text("Degree:\(typesOfDegree[selectedDegree])")
+            HStack{
+                Toggle(isOn: $ButtonSwitch){
+                    Text("I'm a student from FCU").bold()
+                }.padding()
+                .frame(width: 290.0)
+                .background(ButtonSwitch ? Color.yellow :Color.white)
+            }
+            Button("Sign Up"){
+                print("Sign in using \(self.username) and \(self.password)")
+            }.disabled(!enableButton)
+        }
     }
 }
 
