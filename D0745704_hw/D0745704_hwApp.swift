70bab38a0476bff836242094563ad163d39fdda7
D0745704 <mushroom512348@gmail.com>
2021-04-14 17:55:18 +0800

    Initial Commit

---
+++ D0745704_hw/D0745704_hwApp.swift
@@ -0,0 +1,17 @@
+//
+//  D0745704_hwApp.swift
+//  D0745704_hw
+//
+//  Created by 仲輝 on 2021/4/14.
+//
+
+import SwiftUI
+
+@main
+struct D0745704_hwApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
